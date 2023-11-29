7b143ba9d41527a83cb8a4d6f2a499c43042f0c9
jhicks2306 <jhicks2306@gmail.com>
2023-11-29 19:35:54 +0000

    Add equality() expression to Parser

--- jlox/Parser.java
+++ jlox/Parser.java
@@ -21,5 +21,57 @@ class Parser {
         return equality();
     }
 
-    
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private boolean match(TokenType... types) {
+        /* Checks if current token matches those in list provided.
+         If there is a match then consumes the token and returns true.
+         Otherwise false and token not consumed. */
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+        // Returns true if current token is of given type.
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        // Consumes current token and returns it.
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        // Returns current token yet to be consumed.
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        // Returns most recently consumed token.
+        return tokens.get(current - 1);
+    }
+
+
 }
