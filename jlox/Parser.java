5bdb82b1fed31257fabfec1b11ef527510334aaf
jhicks2306 <jhicks2306@gmail.com>
2023-11-29 19:44:08 +0000

    Add binary operators to Parser

--- jlox/Parser.java
+++ jlox/Parser.java
@@ -1,5 +1,6 @@
 package jlox;
 
+import java.time.chrono.MinguoChronology;
 import java.util.List;
 
 import static jlox.TokenType.*;
@@ -27,6 +28,42 @@ class Parser {
         while (match(BANG_EQUAL, EQUAL_EQUAL)) {
             Token operator = previous();
             Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr comparision() {
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr term() {
+        Expr expr = factor();
+
+        while (match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr factor() {
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
             expr = new Expr.Binary(expr, operator, right);
         }
 
