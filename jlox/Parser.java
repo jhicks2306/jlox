bb93c39efd037078437c9c70f660ce13a41669e1
jhicks2306 <jhicks2306@gmail.com>
2023-12-16 08:58:01 +0000

    Add function declarations

--- jlox/Parser.java
+++ jlox/Parser.java
@@ -46,6 +46,7 @@ class Parser {
          * Try block catches parsing errors and goes into error recovery.
          */
         try {
+            if(match(FUN)) return function("function");
             if (match(VAR)) return varDeclaration();
 
             return statement();
@@ -159,6 +160,27 @@ class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private Stmt.Function function(String kind) {
+        Token name = consume(IDENTIFIER, "Expect" + kind + " name.");
+        consume(LEFT_PAREN, "Expect '(' after " + " name.");
+        List<Token> parameters = new ArrayList<>();
+        if (!check(RIGHT_PAREN)) {
+            do  {
+                if (parameters.size() >= 255) {
+                    error(peek(), "Can't have more that 255 parameters.");
+                }
+
+                parameters.add(
+                    consume(IDENTIFIER, "Expect parameter name."));
+            } while (match(COMMA));
+        }
+        consume(RIGHT_PAREN, "Expect ')' after parameters.");
+
+        consume(LEFT_BRACE, "Expect '{' before " + kind + " body.");
+        List<Stmt> body = block();
+        return new Stmt.Function(name, parameters, body);
     }
 
     private List<Stmt> block() {
