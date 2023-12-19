0a50daee5019d95845ea093af9419215364238eb
jhicks2306 <jhicks2306@gmail.com>
2023-12-19 11:24:48 +0000

    Add methods to Resolver

--- jlox/Resolver.java
+++ jlox/Resolver.java
@@ -7,7 +7,7 @@ import java.util.Stack;
 
 class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private final Interpreter interpreter;
-    private final Stack<String, Boolean>> scopes = new Stack<>();
+    private final Stack<Map<String, Boolean>> scopes = new Stack<>();
 
     Resolver(Interpreter interpreter) {
         this.interpreter = interpreter;
@@ -25,6 +25,17 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         stmt.accept(this);
     }
 
+    private void resolveFunction(Stmt.Function function) {
+        // Creates scope for the fn body, binds variables, and resolves fn body.
+        beginScope();
+        for (Token param : function.params) {
+            declare(param);
+            define(param);
+        }
+        resolve(function.body);
+        endScope();
+    }
+
     private void beginScope() {
         scopes.push(new HashMap<String, Boolean>());
     }
@@ -33,9 +44,34 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         scopes.pop();
     }
 
+    private void declare(Token name) {
+        /* Adds variable to innermost scope. Boolean value denotes whether
+         * finished resolving the variable's initializer expression.
+         */
+
+        if (scopes.isEmpty()) return;
+
+        Map<String, Boolean> scope = scopes.peek();
+        scope.put(name.lexeme, false);
+    }
+
+    private void define(Token name) {
+        // Sets variable's value in scope map to 'true' after it's initializer has been resolved.
+        if (scopes.isEmpty()) return;
+        scopes.peek().put(name.lexeme, true);
+    }
+
+    private void resolveLocal(Expr expr, Token name) {
+        for (int i = scopes.size() -1; i >= 0; i--) {
+            if (scopes.get(i).containsKey(name.lexeme)) {
+                interpreter.resolve(expr, scopes.size() - 1 - i);
+            }
+        }
+    }
+
     @Override
     public Void visitBlockStmt(Stmt.Block stmt) {
-        // Begings scope, traverses bloack of statments, then discards the scope.
+        // Begins scope, traverses bloack of statments, then discards the scope.
         beginScope();
         resolve(stmt.statements);
         endScope();
@@ -43,5 +79,43 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitFunctionStmt(Stmt.Function stmt) {
+        // Define fn name in current scope then resolve function.
+        declare(stmt.name);
+        define(stmt.name);
+
+        resolveFunction(stmt);
+        return null;
+    }
+
+
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        declare(stmt.name);
+        if (stmt.initializer != null) {
+            resolve(stmt.initializer);
+        }
+        define(stmt.name);
+        return null;
+    }
+
+    @Override
+    public Void visitAssignExpr(Expr.Assign expr) {
+        resolve(expr.value);
+        resolveLocal(expr, expr.name);
+        return null;
+    }
+
+    @Override
+    public Void visitVariableExpr(Expr.Variable expr) {
+        if (!scopes.isEmpty() &&
+            scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
+                Lox.error(expr.name, "Can't read local variable in its own initializer");
+            }
+
+            resolveLocal(expr, expr.name);
+            return null;
+    }
 
 }
